13f31a36f8dcedcc1bbf7db52dbf0265eb3e8e66
jerni81 <jerni81@yahoo.com>
2020-03-25 16:55:40 -0400

    initial layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react"
+import "../assets/about.css"
 
 const About = () => {
   return (
     <div>
-      About Page
+      <header>
+        <div class="title">About me</div>
+        <div class="tabs">
+          <a>home</a>|<a>about</a>|<a>blog</a>|<a>contact</a>
+        </div>
+      </header>
+      <div class="main">
+        <div class="text">
+          <h3>Hey! I’m Kristina.</h3>
+          <p>
+            Hi and welcome to Kristina Maness Photography! I am a Tampa-based
+            photographer specializing in a crisp, clean, elegant style that
+            highlights the beauty and uniqueness in your family or professional
+            photo shoots. From engagement photos to shooting weddings, and every
+            step of your life journey, we’ll work together to find the perfect
+            touch and make your family shine. My work is also used by clients in
+            their branding and marketing materials and more.
+          </p>
+          <p>
+            As a lifestyle and natural photographer, I love to work with the
+            elegant light of day — especially in the early morning hours right
+            after the sun comes up or during the golden hour just before dusk.
+            That magic gold dust of the beginning and end of day makes for truly
+            beautiful photos of you and your family or loved one. (Insert heart
+            eyes!)
+          </p>
+          <p>
+            Your Tampa family or engagement photo shoot should be fun, fresh and
+            enjoyable for everyone. Whether booking a newborn session or fresh
+            48, annual family photos, or doing a holiday session, I want to
+            capture you being you. That’s what makes each project special. My
+            wedding photography makes your destination wedding or local Tampa
+            event so idyllic and unique to the couple. We sit down beforehand to
+            go over your style and must-haves to ensure perfection from start to
+            finish on your special day.
+          </p>
+          <p>
+            I also am known for my work with professionals — turning head shots
+            into editorial quality shoots and making your business sparkle with
+            my creative touch behind the lens. My marketing and branding
+            photography is done in the same fresh and light style as the rest of
+            my work, bringing an ethereal and gorgeous glow to every project you
+            need.
+          </p>
+          <p>
+            When I’m not photographing clients and editing their favorites to
+            perfection, my hobbies include hanging out with my boyfriend and
+            cat, mixing up cocktails, scouting cool photo shoot locations in and
+            around Tampa, sipping on bourbon and guzzling coffee. Whether
+            booking a family beach vacation photo shoot in Tampa, looking for
+            someone local to become your go-to photographer, or bringing your
+            wedding and engagement pictures to the next level, contact me today
+            and let’s make some memories together that will never go out of
+            style.
+          </p>
+        </div>
+        <div class="pic"></div>
+      </div>
     </div>
   )
 }
