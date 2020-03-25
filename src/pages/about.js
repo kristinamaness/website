bacebd8c0aa079446878e15215c562326b0fe1c5
Jeff Webb <webbwear@icloud.com>
2020-03-25 16:54:14 -0400

    Initialize contact page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react"
 
 const About = () => {
   return (
-    <div>
-      About Page
+    <div class="flex flex-col bg-gray-200">
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">1</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">2</div>
+      <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">3</div>
     </div>
   )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from "react"
+import "../assets/styles/global.css"
 
 const Contact = () => {
   return (
-    <div>
-      Contact Page
-    </div>
+    <form method="post" action="#">
+      <div class="px-4 py-2 m-2">
+        <label>
+          Name
+          <input type="text" name="name" id="name" />
+        </label>
+      </div>
+      <div class="px-4 py-2 m-2">
+        <label>
+          Email
+          <input type="email" name="email" id="email" />
+        </label>
+      </div>
+      <div class="px-4 py-2 m-2">
+        <label>
+          Subject
+          <input type="text" name="subject" id="subject" />
+        </label>
+      </div>
+      <div class="px-4 py-2 m-2">
+        <label>
+          Message
+          <textarea name="message" id="message" rows="5" />
+        </label>
+      </div>
+      <button type="submit">Send</button>
+      <input type="reset" value="Clear" />
+    </form>
   )
 }
 
