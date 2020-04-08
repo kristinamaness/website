74a6d29d7c5a95d44f7f999bc280eed94ab4f67b
Jeff Webb <webbwear@icloud.com>
2020-04-08 16:15:13 -0400

    Nav bar fully responsive

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,50 +5,43 @@ import "../../public/static/fonts/fonts.css"
 const Contact = () => {
   const [open, setOpen] = useState(false)
   return (
-    <header>
+    <header className="sm:flex sm:justify-between sm:items-center ">
       <div className="flex items-center justify-between px-6 py-8">
         <div className="font-semibold">
           <span className="font-header text-4xl pt-10">Contact</span>
         </div>
-        <div>
+        <div className="sm:hidden">
           <button
             onClick={() => setOpen(!open)}
             type="button"
             className="block text-gray-600 hover:text-gray-500 focus:text-gray-500 focus:outline-none"
           >
             {open ? (
-              "X"
+              <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                <path d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"></path>
+              </svg>
             ) : (
-              <svg
-                className="h-6 w-6 fill-current"
-                viewBox="0 0 100 80"
-                width="40"
-                height="40"
-              >
-                <rect width="100" height="10" rx="8"></rect>
-                <rect y="30" width="100" height="10" rx="8"></rect>
-                <rect y="60" width="100" height="10" rx="8"></rect>
+              <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"></path>
               </svg>
             )}
           </button>
         </div>
       </div>
-      <div
-        className={`${open ? "" : "hidden"} px-6 pt-3 pb-5 mobile-drawer-bg`}
-      >
-        <a href="#" className="mt-2 block hover:text-white">
+      <nav className={`${open ? "" : "hidden"} pt-3 pb-5 sm:flex`}>
+        <a href="#" className="mt-2 px-6 block">
           home
         </a>
-        <a href="#" className="mt-2 block hover:text-white">
+        <a href="#" className="mt-2 px-6 block">
           about
         </a>
-        <a href="#" className="mt-2 block hover:text-white">
+        <a href="#" className="mt-2 px-6 block">
           blog
         </a>
-        <a href="#" className="mt-2 block hover:text-white">
+        <a href="#" className="mt-2 px-6 block">
           contact
         </a>
-      </div>
+      </nav>
     </header>
   )
 }
